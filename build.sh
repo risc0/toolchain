89d5b20764db20047805275619cc1a9f76b87a08
Frank Laub <frank@risczero.com>
2024-01-05 21:45:05 -0800

    Use riscv-gnu-toolchain that uses newlib for Linux ABI (#2)

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-pushd crosstool-ng
+HOST=$1
+PREFIX=$PWD/.dist/$HOST
 
-./bootstrap
-./configure --enable-local
-make
+pushd riscv-gnu-toolchain
+
+./configure --prefix=$PREFIX --with-cmodel=medany --disable-gdb --with-arch=rv32im --with-abi=ilp32
+make -j$(nproc 2>/dev/null || sysctl -n hw.logicalcpu)
+
+popd
+
+pushd .dist
+
+tar cvJf $HOST.tar.xz $HOST
 
 popd
